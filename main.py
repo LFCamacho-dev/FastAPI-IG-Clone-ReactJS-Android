58d497dbbc9411cb4e1c965408b3c8146d38cb06
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-05-08 12:46:53 -0500

    create posts, get all posts

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from sqlalchemy.sql.functions import user
 import uvicorn
 from db import models
 from db.database import engine
-from routers import user
+from routers import user, post
 
 
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(post.router)
 
 
 
--- routers/schemas.py
+++ routers/schemas.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from lib2to3.pytree import Base
 from pydantic import BaseModel
 # from datetime import datetime
 # from typing import List
@@ -13,4 +15,28 @@ class UserDisplay(BaseModel):
   username: str
   email: str
   class Config():
+      orm_mode = True
+      
+    
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str
+    caption: str
+    creator_id: int
+    
+    
+class User(BaseModel):
+    username: str
+    class Config():
+      orm_mode = True
+
+
+class PostDisplay(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: datetime
+    user: User
+    class Config():
       orm_mode = True
